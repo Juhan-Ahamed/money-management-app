d90211310aebf02a05cf7da884780c6b0a6d35bd
Juhan Ahamed <59272211+Juhan-Ahamed@users.noreply.github.com>
2021-04-26 12:13:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -38,13 +38,11 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`)
-    setTimeout(() => { 
     mongoose.connect(uri,
         { useNewUrlParser: true, useUnifiedTopology: true },
         () => {
-            console.log('Database Connected...');
-        });
-    }, 60000)
+            console.log('Database Connected...')
+        })
 })
 
 //mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@cluster0.xcq4x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
